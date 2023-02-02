c37a19ce68b6e9421577ef3df0efec15da68cb6d
“Ivan <ivkriv@yandex.ru>
2023-02-02 15:29:38 +0300

    resheno2

--- zadacha10/Program.cs
+++ zadacha10/Program.cs
@@ -2,3 +2,16 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+System.Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a > 99 && a < 1000)
+{
+    int s = a/10%10;
+    System.Console.WriteLine(s);
+}
+else
+{
+    System.Console.WriteLine("Число не трехзначное!");
+}
